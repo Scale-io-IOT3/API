ffa89f40741acb5deb03fef794c1e9233b1bc9d5
proudhAteR <158060444+proudhAteR@users.noreply.github.com>
2025-09-24 11:48:06 -0400

    feat: added the watch feature back to the docker in dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 # Initialize DB if it doesn't exist
 if [ ! -f /app/data/app.db ]; then
@@ -7,9 +8,4 @@ if [ ! -f /app/data/app.db ]; then
   sqlite3 /app/data/app.db < /app/data/init.sql
 fi
 
-# Decide command based on environment variable
-if [ "$DOTNET_DEV" = "true" ]; then
-  exec dotnet watch run --project API/API.csproj --urls http://0.0.0.0:8080
-else
-  exec dotnet API.dll
-fi
+exec "$@"
